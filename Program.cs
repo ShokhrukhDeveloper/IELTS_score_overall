7149fb2b31e4e8034ef48e227835a3f5c07ffeb6
Shoxrux <shoxruxyarashovuz@gmail.com>
2024-02-12 23:17:09 +0500

    intial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("IELTS score average FOR academic");
+
+Console.Write("Listening score: ");
+string listeningScoreInput = Console.ReadLine()!;
+
+Console.Write("Reading score: ");
+string readingScoreInput = Console.ReadLine()!;
+
+Console.Write("Speaking score: ");
+string speakingScoreInput = Console.ReadLine()!;
+
+Console.Write("Writing score: ");
+string writingScoreInput = Console.ReadLine()!;
+
+decimal[] scores={
+    Convert.ToDecimal(readingScoreInput),
+    Convert.ToDecimal(listeningScoreInput),
+    Convert.ToDecimal(writingScoreInput),
+    Convert.ToDecimal(speakingScoreInput)
+    };
+
+decimal averageScore = scores.Average();
+decimal remainder = averageScore-(int)averageScore;
+
+remainder = remainder switch
+{
+    <   0.25m => 0m,
+    <   0.75m => 0.5m,
+    _         => 1m
+};
+
+averageScore=(int)averageScore+remainder;
+string scoreCategory= averageScore switch 
+{
+  9 or 8.5m => "Expert",
+  8 or 7.5m => "Very Good",
+  7 or 6.5m => "Good",
+  6 or 5.5m => "Competent",
+  5 => "Modest",
+  _ => "Invalid" 
+};
+
+Console.WriteLine("------------------------------------------------------------");
+Console.WriteLine($"Avreage score: {averageScore}");
+Console.WriteLine($"Candidat is {scoreCategory}");
